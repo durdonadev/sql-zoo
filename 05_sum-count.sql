8fe0d10f31e5a1fb473f71a36ec15eb675365569
Durdona Ibragimova <durdona.dev@gmail.com>
2024-05-05 13:57:12 -0400

    Solve sql challanges

--- 05_sum-count.sql
+++ 05_sum-count.sql
@@ -1,3 +1,17 @@
 -- https://sqlzoo.net/wiki/SUM_and_COUNT
 -- world(name, continent, area, population, gdp);
 --  --  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- 1: 
+-- Show the total population of the world.
+SELECT
+    SUM(population) AS world_population
+FROM
+    world;
+
+-- 2:
+-- List all the continents - just once each.
+SELECT DISTINCT
+    (continent)
+FROM
+    world;
