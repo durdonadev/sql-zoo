d1e8c1fdd9df88c54abe1dbbf512026655986383
Durdona Ibragimova <durdona.dev@gmail.com>
2024-05-05 13:57:44 -0400

    Solve sql challanges in 05_sum-count

--- 05_sum-count.sql
+++ 05_sum-count.sql
@@ -15,3 +15,21 @@ SELECT DISTINCT
     (continent)
 FROM
     world;
+
+-- 3:
+-- Give the total GDP of Africa
+SELECT
+    SUM(gdp) AS africa_gdp
+FROM
+    world
+WHERE
+    continent = "Africa";
+
+-- 4: 
+-- How many countries have an area of at least 1000000
+SELECT
+    COUNT(area) AS big_countries
+FROM
+    world
+WHERE
+    area >= 1000000;
