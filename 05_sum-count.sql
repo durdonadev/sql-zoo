4e4a4482729fe9bebd6da49bb119e0fc2a3d8a7a
Durdona Ibragimova <durdona.dev@gmail.com>
2024-05-07 22:43:53 -0400

    Solve sql challanges from 05_sum-count

--- 05_sum-count.sql
+++ 05_sum-count.sql
@@ -33,3 +33,34 @@ FROM
     world
 WHERE
     area >= 1000000;
+
+    -- 5: 
+-- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+SELECT
+    SUM(population) AS baltic_population
+FROM
+    world
+WHERE
+    name IN ('Estonia', 'Latvia', 'Lithuania');
+
+-- 6:
+-- For each continent show the continent and number of countries.
+SELECT
+    continent,
+    COUNT(name) AS number_of_countries
+FROM
+    world
+GROUP BY
+    continent;
+
+-- 7: 
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT
+    continent,
+    COUNT(name) AS number_of_big_countries
+FROM
+    world
+WHERE
+    population >= 10000000
+GROUP BY
+    continent;
